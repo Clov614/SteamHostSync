f9a4475d1b4f6c1bf7439999eecbd65fee7cc585
Kirino <541982545@qq.com>
2022-04-24 01:10:16 +0800

    fix: Fix time.In() panic

--- fileIO/Read_tmp.go
+++ fileIO/Read_tmp.go
@@ -2,13 +2,14 @@ package fileIO
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 func Read_tmp() string {
 	file, err := os.Open("./README_TEMP.md")
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 	defer file.Close()
 	content, err := ioutil.ReadAll(file)
